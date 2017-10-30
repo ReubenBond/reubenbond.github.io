6026842334ea3b59fcb969519651caf190de235c
Reuben Bond <a-rebond@microsoft.com>
2017-10-31 08:05:59 +1100

    publish

---
+++ publish.ps1
@@ -0,0 +1,18 @@
+param (
+  [string]$Token,
+  [string]$UserName,
+  [string]$Repository
+)
+
+$localFolder = "gh-pages"
+git clone https://$($Token)@github.com/$($Repository).git --branch=gh-pages $localFolder
+
+$from = "$(Build.Repository.LocalPath)\output\*"
+$to = "$(Build.Repository.LocalPath)\$($localFolder)"  
+Copy-Item $from $to -recurse
+
+Push-Location $localFolder
+git add *
+git commit -m "Update."
+git push
+Pop-Location
