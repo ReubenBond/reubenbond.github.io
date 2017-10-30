0a318950281e9f206416f5831f60e98811a5a48d
Reuben Bond <a-rebond@microsoft.com>
2017-10-31 08:53:12 +1100

    publish

--- publish.ps1
+++ publish.ps1
@@ -6,17 +6,13 @@ param (
 
 $localFolder = "gh-pages"
 $repo = "https://$($UserName):$($Token)@github.com/$($Repository).git"
-git clone $repo --branch=gh-pages $localFolder
+git clone $repo --branch=master $localFolder
 
 $from = "output\*"
 $to = $($localFolder)
 Copy-Item $from $to -recurse
 
 Push-Location $localFolder
-Write-Host "adding"
 git add *
-Write-Host "committing"
 git commit -m "Update."
-Write-Host "pushing to '$repo'"
-git push --repo=$repo -f -v
-Write-Host "done"
+git push --repo=$repo -f
